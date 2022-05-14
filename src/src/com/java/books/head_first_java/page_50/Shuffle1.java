37e3d26d7f78da269e3918fe14b3fb0bb9cb9e93
mimiPIllow <erenstonxemin@gmail.com>
2022-05-14 19:52:37 +0300

    head first java (page 50)

---
+++ src/src/com/java/books/head_first_java/page_50/Shuffle1.java
@@ -0,0 +1,21 @@
+package com.java.books.head_first_java.page_50;
+
+public class Shuffle1 {
+    public static void main(String[] args) {
+        int x = 3;
+        while (x > 0) {
+            if (x > 2) {
+                System.out.print("a");
+            }
+            if (x == 2) {
+                System.out.print("b c");
+            }
+            x--;
+            System.out.print("-");
+            if (x == 1) {
+                System.out.print("d");
+                x--;
+            }
+        }
+    }
+}
