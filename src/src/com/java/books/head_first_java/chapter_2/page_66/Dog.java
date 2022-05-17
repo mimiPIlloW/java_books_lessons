511217dc82c9b24e0178c13459fbe8f996c98783
mimiPIllow <erenstonxemin@gmail.com>
2022-05-17 14:00:54 +0300

    head first java chapter 2 (page 66)

---
+++ src/src/com/java/books/head_first_java/chapter_2/page_66/Dog.java
@@ -0,0 +1,11 @@
+package com.java.books.head_first_java.chapter_2.page_66;
+
+public class Dog {
+    int size;
+    String breed;
+    String name;
+
+    void bark(){
+        System.out.println("Gav! Gav!");
+    }
+}
---
+++ src/src/com/java/books/head_first_java/chapter_2/page_66/DogTestDrive.java
@@ -0,0 +1,9 @@
+package com.java.books.head_first_java.chapter_2.page_66;
+
+public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+    }
+}
