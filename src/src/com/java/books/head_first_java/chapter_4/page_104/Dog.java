1d5c82ed66c6e42a6a8fc3b8d32d95e09589659c
mimiPIllow <erenstonxemin@gmail.com>
2022-05-20 14:30:56 +0300

    head first java chapter 4 (page 104)

---
+++ src/src/com/java/books/head_first_java/chapter_4/page_104/Dog.java
@@ -0,0 +1,10 @@
+package com.java.books.head_first_java.chapter_4.page_104;
+
+public class Dog {
+    public void bark(int numOfBarks) {
+        while (numOfBarks > 0) {
+            System.out.println("Гав");
+            numOfBarks--;
+        }
+    }
+}
---
+++ src/src/com/java/books/head_first_java/chapter_4/page_104/DogTestDrive.java
@@ -0,0 +1,8 @@
+package com.java.books.head_first_java.chapter_4.page_104;
+
+public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog one = new Dog();
+        one.bark(3);
+    }
+}
