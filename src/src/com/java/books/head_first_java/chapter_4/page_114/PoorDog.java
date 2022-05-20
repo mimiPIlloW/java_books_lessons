9cffdf4f0f7fd7690e562002d540028b0951031f
mimiPIllow <erenstonxemin@gmail.com>
2022-05-20 14:32:09 +0300

    head first java chapter 4 (page 114)

---
+++ src/src/com/java/books/head_first_java/chapter_4/page_114/PoorDog.java
@@ -0,0 +1,14 @@
+package com.java.books.head_first_java.chapter_4.page_114;
+
+public class PoorDog {
+    private int size;
+    private String name;
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/src/com/java/books/head_first_java/chapter_4/page_114/PoorDogTestDrive.java
@@ -0,0 +1,9 @@
+package com.java.books.head_first_java.chapter_4.page_114;
+
+public class PoorDogTestDrive {
+    public static void main(String[] args) {
+        PoorDog poorDog = new PoorDog();
+        System.out.println(poorDog.getName());
+        System.out.println(poorDog.getSize());
+    }
+}
