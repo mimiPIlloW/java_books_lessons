a4ab0548bbe5330572da554c3eea53a4c1c3c448
mimiPIllow <erenstonxemin@gmail.com>
2022-05-27 12:42:48 +0300

    head first java chapter 5 (page 151 - e)

---
+++ src/src/com/java/books/head_first_java/chapter_5/page_151/e/MixFor5.java
@@ -0,0 +1,20 @@
+package com.java.books.head_first_java.chapter_5.page_151.e;
+
+public class MixFor5 {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 30;
+        for (int outer = 0; outer < 3; outer++) {
+            for (int inner = 4; inner > 1; inner--) {
+                x--;
+                y -= 2;
+                if (x == 6) {
+                    break;
+                }
+                x += 3;
+            }
+            y -= 2;
+        }
+        System.out.println(x + " " + y);
+    }
+}
