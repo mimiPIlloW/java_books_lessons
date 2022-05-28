9c0e50232b2bccbbbb4d46187edd5d5057c57ff2
mimiPIllow <erenstonxemin@gmail.com>
2022-05-28 13:52:08 +0300

    java 8 beginners guide chapter 1 (page 37)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_1/page_37/Example.java
@@ -0,0 +1,7 @@
+package com.java.books.java8.beginners_guide.chapter_1.page_37;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Java правит Интернетом");
+    }
+}
