beab2b1680b57b48ab3f6ac2577d7794d31516c4
mimiPIllow <erenstonxemin@gmail.com>
2022-05-28 13:53:59 +0300

    java 8 beginners guide chapter 1 (page 49)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_1/page_49/ForDemo.java
@@ -0,0 +1,12 @@
+package com.java.books.java8.beginners_guide.chapter_1.page_49;
+
+public class ForDemo {
+    public static void main(String[] args) {
+        int count;
+
+        for (count = 0; count < 5; count++) {
+            System.out.println("Знaчeниe счетчика: " + count);
+        }
+        System.out.println("Готово");
+    }
+}
