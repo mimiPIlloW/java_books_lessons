941e06a39a5989ea9d2049c0a04959abdaa54014
mimiPIllow <erenstonxemin@gmail.com>
2022-05-28 13:53:09 +0300

    java 8 beginners guide chapter 1 (page 46)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_1/page_46/GalToLit.java
@@ -0,0 +1,15 @@
+package com.java.books.java8.beginners_guide.chapter_1.page_46;
+
+public class GalToLit {
+
+    public static void main(String[] args) {
+        double gallons;
+        double liters;
+
+        gallons = 10;
+
+        liters = gallons * 3.7854;
+
+        System.out.println(gallons + " галлонам соответствует " + liters + " литров");
+    }
+}
