583a02b2b763fb3da3cf61e164850666f034271a
mimiPIllow <erenstonxemin@gmail.com>
2022-05-28 13:52:53 +0300

    java 8 beginners guide chapter 1 (page 44)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_1/page_44/Example3.java
@@ -0,0 +1,21 @@
+package com.java.books.java8.beginners_guide.chapter_1.page_44;
+
+public class Example3 {
+    public static void main(String[] args) {
+        int var;
+        double x;
+
+        var = 10;
+        x = 10.0;
+
+        System.out.println("Haчaльнoe значение переменной var: " + var);
+        System.out.println("Haчaльнoe значение переменной х: " + x);
+        System.out.println();
+
+        var /= 4;
+        x /=4;
+
+        System.out.println("Знaчeниe переменной var после деления: " + var);
+        System.out.println("Знaчeниe переменной х после деления: "+ x);
+    }
+}
