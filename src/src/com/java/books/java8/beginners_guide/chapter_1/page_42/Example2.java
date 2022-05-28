f058ab9827371cd617a604c6df986b573c7769d8
mimiPIllow <erenstonxemin@gmail.com>
2022-05-28 13:52:27 +0300

    java 8 beginners guide chapter 1 (page 42)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_1/page_42/Example2.java
@@ -0,0 +1,14 @@
+package com.java.books.java8.beginners_guide.chapter_1.page_42;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int var1;
+        int var2;
+
+        var1 = 1024;
+        System.out.println("Var1: " + var1);
+
+        var2 = var1 / 2;
+        System.out.println("Var2: " + var2);
+    }
+}
