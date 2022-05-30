17206462588c54541524e7d9655f6a9667867af6
mimiPIllow <erenstonxemin@gmail.com>
2022-05-30 13:46:16 +0300

    java 8 beginners guide chapter 2 (page 69)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_2/page_69/DynInit.java
@@ -0,0 +1,11 @@
+package com.java.books.java8.beginners_guide.chapter_2.page_69;
+
+public class DynInit {
+    public static void main(String[] args) {
+        double radius = 4, height = 5;
+
+        double volume = 3.1416 * radius * radius * height;
+
+        System.out.println(volume);
+    }
+}
