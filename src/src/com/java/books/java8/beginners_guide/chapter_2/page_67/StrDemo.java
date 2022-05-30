1b06480da4cec3f66c837932c364f51973fa308f
mimiPIllow <erenstonxemin@gmail.com>
2022-05-30 13:46:09 +0300

    java 8 beginners guide chapter 2 (page 67)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_2/page_67/StrDemo.java
@@ -0,0 +1,9 @@
+package com.java.books.java8.beginners_guide.chapter_2.page_67;
+
+public class StrDemo {
+    public static void main(String[] args) {
+        System.out.println("Пepвaя строка\nВторая строка");
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+    }
+}
