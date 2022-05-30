670b8d279858c7f07e816bcf810f9bb9ffae5811
mimiPIllow <erenstonxemin@gmail.com>
2022-05-30 13:46:00 +0300

    java 8 beginners guide chapter 2 (page 64)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_2/page_64/Sound.java
@@ -0,0 +1,11 @@
+package com.java.books.java8.beginners_guide.chapter_2.page_64;
+
+public class Sound {
+    public static void main(String[] args) {
+        double dist;
+
+        dist = 1100 * 7.2;
+
+        System.out.println("Paccтoяниe до места вспышки молнии " + "составляет "+ dist + " футов");
+    }
+}
