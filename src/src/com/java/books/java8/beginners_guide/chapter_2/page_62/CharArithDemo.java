ae12d7488773497e061da5eb586ded64d2369ca2
mimiPIllow <erenstonxemin@gmail.com>
2022-05-30 13:45:36 +0300

    java 8 beginners guide chapter 2 (page 62)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_2/page_62/CharArithDemo.java
@@ -0,0 +1,17 @@
+package com.java.books.java8.beginners_guide.chapter_2.page_62;
+
+public class CharArithDemo {
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'Æ';
+        System.out.println("ch содержит " + ch);
+
+        ch++;
+        System.out.println("тenepь ch содержит " + ch);
+
+        ch = 90;
+        System.out.println("тenepь ch содержит " + ch);
+
+    }
+}
