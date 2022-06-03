dbc3f5c698a1df5d01a38e0f9938d8a1c2111245
mimiPIllow <erenstonxemin@gmail.com>
2022-06-03 11:56:42 +0300

    java 8 beginners guide chapter 3 (page 107)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_3/page_107/WhileDemo.java
@@ -0,0 +1,13 @@
+package com.java.books.java8.beginners_guide.chapter_3.page_107;
+
+public class WhileDemo {
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'a';
+        while (ch <= 'z') {
+            System.out.print(ch + " ");
+            ch++;
+        }
+    }
+}
