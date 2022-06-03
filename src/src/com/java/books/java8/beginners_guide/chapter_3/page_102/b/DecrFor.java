ccf55059beb5a4b277eb63376761f78df97af21c
mimiPIllow <erenstonxemin@gmail.com>
2022-06-03 11:55:13 +0300

    java 8 beginners guide chapter 3 (page 102 - b)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_3/page_102/b/DecrFor.java
@@ -0,0 +1,11 @@
+package com.java.books.java8.beginners_guide.chapter_3.page_102.b;
+
+public class DecrFor {
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 100; x >= -100; x -= 5) {
+            System.out.println(x);
+        }
+    }
+}
