549d47e1d352eeb841d51fed8a54088c8310cbb9
mimiPIllow <erenstonxemin@gmail.com>
2022-06-03 11:55:53 +0300

    java 8 beginners guide chapter 3 (page 104 - a)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_3/page_104/a/Empty.java
@@ -0,0 +1,12 @@
+package com.java.books.java8.beginners_guide.chapter_3.page_104.a;
+
+public class Empty {
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 0; i < 10; ) {
+            System.out.println("Проход #" + i);
+            i++;
+        }
+    }
+}
