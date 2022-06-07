d1baaea44517567346b71fd661a779c9e7ac1f5a
mimiPilloW <erenstonxemin@gmail.com>
2022-06-07 11:17:17 +0300

    java 8 beginners guide chapter 4 (page 137)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_4/page_137/A.java
@@ -0,0 +1,11 @@
+package com.java.books.java8.beginners_guide.chapter_4.page_137;
+
+public class A {
+    void myMeth() {
+        int i;
+        for (i = 0; i < 10; i++) {
+            if (i == 5) return;
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/src/com/java/books/java8/beginners_guide/chapter_4/page_137/B.java
@@ -0,0 +1,8 @@
+package com.java.books.java8.beginners_guide.chapter_4.page_137;
+
+public class B {
+    public static void main(String[] args) {
+        A a = new A();
+        a.myMeth();
+    }
+}
