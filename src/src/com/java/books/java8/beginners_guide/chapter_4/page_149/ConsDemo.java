aeaaf14620db60efd4b88f116d999138657dadc0
mimiPilloW <erenstonxemin@gmail.com>
2022-06-10 13:03:59 +0300

    java 8 beginners guide chapter 4 (page 149)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_4/page_149/ConsDemo.java
@@ -0,0 +1,10 @@
+package com.java.books.java8.beginners_guide.chapter_4.page_149;
+
+public class ConsDemo {
+    public static void main(String[] args) {
+        MyClass t1 = new MyClass();
+        MyClass t2 = new MyClass();
+
+        System.out.println(t1.x + " " + t2.x);
+    }
+}
---
+++ src/src/com/java/books/java8/beginners_guide/chapter_4/page_149/MyClass.java
@@ -0,0 +1,9 @@
+package com.java.books.java8.beginners_guide.chapter_4.page_149;
+
+public class MyClass {
+    int x;
+
+    MyClass() {
+        x = 10;
+    }
+}
