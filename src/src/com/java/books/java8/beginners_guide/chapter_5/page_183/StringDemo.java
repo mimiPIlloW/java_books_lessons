b9f41f1fed9d9f0cc57e424586bed456768c2aac
mimiPilloW <erenstonxemin@gmail.com>
2022-06-20 09:16:22 +0300

    java 8 beginners guide chapter 5 (page 183)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_5/page_183/StringDemo.java
@@ -0,0 +1,13 @@
+package com.java.books.java8.beginners_guide.chapter_5.page_183;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String str = new String("Привет");
+        String str2 = new String(str);
+        String str3 = "Хай";
+
+        System.out.println(str);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
