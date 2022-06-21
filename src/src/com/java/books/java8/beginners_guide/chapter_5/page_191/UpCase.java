16247d5c3a68e740a304724a6a1ccc347e19418a
mimiPilloW <erenstonxemin@gmail.com>
2022-06-21 09:01:03 +0300

    java 8 beginners guide chapter 5 (page 191)

---
+++ src/src/com/java/books/java8/beginners_guide/chapter_5/page_191/UpCase.java
@@ -0,0 +1,15 @@
+package com.java.books.java8.beginners_guide.chapter_5.page_191;
+
+public class UpCase {
+    public static void main(String[] args) {
+        char ch;
+
+        for (int i = 0; i < 10; i++){
+            ch = (char) ('a' + i);
+            System.out.print(ch);
+
+            ch = (char) ((int) ch & 65503);
+            System.out.print(ch + " ");
+        }
+    }
+}
