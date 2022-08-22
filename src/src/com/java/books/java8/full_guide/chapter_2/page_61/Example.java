f89291dcdf086873bbe3efd8946e2f5ce5d5b4a7
mimiPillow <erenstonxemin@gmail.com>
2022-08-22 11:56:08 +0300

    java 8 full guide chapter 2 (page 61)

---
+++ src/src/com/java/books/java8/full_guide/chapter_2/page_61/Example.java
@@ -0,0 +1,7 @@
+package com.java.books.java8.full_guide.chapter_2.page_61;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Just do it");
+    }
+}
