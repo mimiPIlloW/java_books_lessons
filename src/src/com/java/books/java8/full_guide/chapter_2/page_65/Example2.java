b051162e231fcad81942cb10d25dbda18e604f1f
mimiPillow <erenstonxemin@gmail.com>
2022-08-22 11:56:33 +0300

    java 8 full guide chapter 6 (page 65)

---
+++ src/src/com/java/books/java8/full_guide/chapter_2/page_65/Example2.java
@@ -0,0 +1,15 @@
+package com.java.books.java8.full_guide.chapter_2.page_65;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num;
+
+        num = 100;
+
+        System.out.println(num + " 1");
+
+        num = num * 2;
+
+        System.out.println(num + " 2");
+    }
+}
